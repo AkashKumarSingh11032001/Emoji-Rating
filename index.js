cea99885f6d4936de756a2919bf5d329d817c751
Akash <akashkumarsingh11032001@gmail.com>
2022-08-14 09:58:03 +0530

    completed

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const starsEl = document.querySelectorAll(".fa-star")
-console.log(starsEl)
+// console.log(starsEl)
+const emojisEl = document.querySelectorAll(".far")
+// console.log(emojisEl)
 
-starsEl.forEach((x,index)=>{
-    x.addEventListener("click",()=>{
-       updateRating(index)
+starsEl.forEach((x, index) => {
+    x.addEventListener("click", () => {
+        updateRating(index)
     });
 });
 
-function updateRating(index){
-    starsEl.forEach((X,idx)=>{
-        if()
+function updateRating(index) {
+    starsEl.forEach((x, idx) => {
+        // adding active class to all prev star before index+1
+        if (idx < index + 1) {
+            x.classList.add("active");
+        } else {
+            x.classList.remove("active");
+        }
+    });
+    emojisEl.forEach((y) => {
+        y.style.transform = `translateX(-${index * 50}px)`;
     });
 }
